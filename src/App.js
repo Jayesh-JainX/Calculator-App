6d4c3da8f73a709d4f724eba771521a68c21ee7f
Jayesh Jain <jainjayesh799@gmail.com>
2024-01-09 21:30:32 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function reducer(state, { type, payload }) {
         return state;
       }
 
-      if (payload.digit === "." && state.currentOperand.includes(".")) {
+      if (payload.digit === "." && state.currentOperand === ".") {
         return state;
       }
 
